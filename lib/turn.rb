98ea27b3798205183532d9994a66706455b45e28
Michael Fahim <michael_fahim@yahoo.com>
2019-11-14 01:51:47 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,33 +9,38 @@ puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-
+def valid_move?(board, index)
+  if !position_taken?(board, index) && index.between?(0, 8)
+    return true
+  else
+    return false
+  end
+end
 
 def move(board,index, token)
   board[index] = token
 end
 
-def input_to_index(move)
-   index = move.to_i - 1
-   index
+def input_to_index(user_input)
+   index = user_input.to_i - 1
+   return index
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
-    #get the user input
-    user_input = gets.strip
-    #input to index
-    index = input_to_index(user_input)
-    token = current_player(board)
-
-    #check for validation
-    if valid_move?(board,index)
-      puts "valid move"
-      move(board, index, token)
-      display_board(board)
-     else
-      puts "try again"
-      turn(board)
-    end
-    display_board(board)
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+     return false
+  elsif board[index] == "X" || board[index] == "O"
+     return true
   end
+end
+
+def turn
+  puts "Please enter 1-9:"
+  user_input = get.strip
+  input_to_index(user_input)
+  if valid_move?(index)
+    move(board, index, token)
+  else
+    puts "Please enter 1-9:"
+
+end
